b3d155367844bdf2873fc4e145dc73e86c10e76b
krzysiu <krzysiu.offical@wp.pl>
2022-04-22 12:33:42 +0200

    wstawianie daty w teskt

--- app/src/main/java/com/example/calendar/MainActivity.kt
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.calendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.CalendarView
 import com.example.calendar.databinding.ActivityMainBinding
+import java.text.DateFormat
 import java.util.*
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +19,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.kalendarz.setMinDate(System.currentTimeMillis());
+
+
+
+        binding.button.setOnClickListener(View.OnClickListener {
+            val calendar = Calendar.getInstance()
+            binding.kalendarz.date = calendar.timeInMillis
+            val dateFormatter = DateFormat.getDateInstance(DateFormat.MEDIUM)
+            binding.textwyjazd.append(dateFormatter.format(calendar.time))
+            val selectedDate:Long = binding.kalendarz.date
+            calendar.timeInMillis = selectedDate
+
+        })
     }
 
 }
